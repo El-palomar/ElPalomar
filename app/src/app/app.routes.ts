72cce8e8d73e7f7e018950fea022a9c72ddf4e05
Fabrizio Adrian Avila <109549522+Fabrizio112@users.noreply.github.com>
2025-08-12 23:33:44 -0300

    Crea base del proyecto de Angular y termina vista Home

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+<<<<<<< HEAD
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 
@@ -7,4 +8,9 @@ export const routes: Routes = [
     {path:"",component:Home},
     {path:"login",component:Login},
     {path:"register", component:Register}
+=======
+
+export const routes: Routes = [
+    {path:"",component:Home}
+>>>>>>> 92ac105 (Crea base del proyecto de Angular y termina vista Home)
 ];
--- app/src/app/pages/home/home.ts
+++ app/src/app/pages/home/home.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { ActividadesHome } from '../../components/actividades-home/actividades-home';
 import { Beneficios } from '../../components/beneficios/beneficios';
 import { Main } from '../../components/main/main';
+<<<<<<< HEAD
 import { Footer } from '../../components/footer/footer';
 import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-home',
   imports: [Header, Footer, ActividadesHome,Beneficios,Main],
+=======
+
+@Component({
+  selector: 'app-home',
+  imports: [ActividadesHome,Beneficios,Main],
+>>>>>>> 92ac105 (Crea base del proyecto de Angular y termina vista Home)
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
