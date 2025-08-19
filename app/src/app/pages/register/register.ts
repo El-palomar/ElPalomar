9872e7dd2390722c8214e76a9cc7d101407c078f
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-08-19 12:15:48 -0300

    feat: Migracion de register.html

--- app/src/app/pages/register/register.ts
+++ app/src/app/pages/register/register.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RegisterForm } from '../../components/register-form/register-form';
+import { Header } from '../../components/header/header';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [Header, Footer,RegisterForm],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
