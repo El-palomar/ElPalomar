b4b4ffd245a4364f9a05d44968525be6191243e8
Lis <168233558+marialis19@users.noreply.github.com>
2025-08-20 01:44:07 -0300

    Agrego dashboard

---
+++ app/src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
+import { Header } from '../../components/header/header';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard {
+
+}
