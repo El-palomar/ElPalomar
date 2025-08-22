7c92dae8a61702e22bf4f991e20574cbc2aac43d
Ana Camila <almironanacamila@gmail.com>
2025-08-21 21:14:13 -0300

    feat: agregado de validaciones en formulario

--- app/src/app/pages/login/login.ts
+++ app/src/app/pages/login/login.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
-import { LoginForm } from '../../components/login-form/login-form';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginFormComponent } from '../../components/login-form/login-form';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginForm],
+  imports: [CommonModule, LoginFormComponent],
   templateUrl: './login.html',
-  styleUrls: ['./login.css'],
 })
-export class Login {
-  constructor(private router: Router) {}
-
-  onSubmit(event: { usuario: string; contrasena: string }) {
-    console.log('Usuario:', event.usuario);
-    console.log('Contraseña:', event.contrasena);
-
-    this.router.navigate(['/dashboard']);
-  }
-}
+export class LoginPage {}
