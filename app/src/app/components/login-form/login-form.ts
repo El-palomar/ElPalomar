d30457669bd9624fda02df59c1a14085dbde3a0b
Ana Camila <almironanacamila@gmail.com>
2025-08-23 11:17:30 -0300

    fix: correcciones en angular y de portafolio

--- app/src/app/components/login-form/login-form.ts
+++ app/src/app/components/login-form/login-form.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule], // 👈 IMPORTANTE
   templateUrl: './login-form.html',
   styleUrl: './login-form.css',
 })
 export class LoginFormComponent {
   form;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -27,6 +28,7 @@ export class LoginFormComponent {
   onSubmit() {
     if (this.form.valid) {
       console.log('Login enviado ', this.form.value);
+      this.router.navigate(['/dashboard']);
     } else {
       this.form.markAllAsTouched();
     }
