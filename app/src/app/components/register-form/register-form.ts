1f200c1c6283d927af6e2a0f1aa4cbdbb3202342
Fabrizio Adrian Avila <109549522+Fabrizio112@users.noreply.github.com>
2025-08-23 18:19:54 -0300

    FIX - Corrige estilos
    Se corrigen estilos del login/register.
    Se cambia el logo del navbar

--- app/src/app/components/register-form/register-form.ts
+++ app/src/app/components/register-form/register-form.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,RouterModule],
   templateUrl: './register-form.html',
   styleUrls: ['./register-form.css']
 })
