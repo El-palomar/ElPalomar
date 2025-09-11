89f67291301400fe2f5232829c1d30d0497204dc
Fabrizio Adrian Avila <109549522+Fabrizio112@users.noreply.github.com>
2025-09-11 11:47:05 -0300

    FEAT - Desarrolla el servicio de actividades y usuario ,ademas del modelo
    Se desarrollan 2 servicios :
    Uno para consumir los datos del json de las actividades del club y otro servicio para consumir los datos del json de las actividades inscriptas por el usuario
    A su vez se desarrollan los modelos correspondientes a estas 2 actividades para tipar de forma correcta las respuestas de las peticiones http.

--- app/src/app/components/actividades-home/actividades-home.ts
+++ app/src/app/components/actividades-home/actividades-home.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { IActivity } from '../../models/activity';
 
 @Component({
   selector: 'app-actividades-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './actividades-home.html',
   styleUrl: './actividades-home.css'
 })
 export class ActividadesHome {
+  @Input() activities: IActivity[] = []
 
 }
--- app/src/app/pages/dashboard/dashboard.ts
+++ app/src/app/pages/dashboard/dashboard.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Footer } from '../../components/footer/footer';
-import { Header } from '../../components/header/header';
 import { RouterModule } from '@angular/router';
 import { Sidebar } from '../../components/sidebar/sidebar';
+import { ActivitiesService } from '../../services/activities';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user';
+import { IActivity } from '../../models/activity';
+import { IUserActivity } from '../../models/user-activity';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [Sidebar,Footer,RouterModule],
+  imports: [Sidebar, Footer, RouterModule, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  activities: IActivity[] = []
+  userActivities: IUserActivity[] = []
+  constructor(private activityService: ActivitiesService, private userActivityService: UserService) { }
+
+  ngOnInit() {
+    this.getInitialData()
+  }
+  getInitialData() {
+    this.activityService.getAllActivities().subscribe({
+      next: (activities) => {
+        this.activities = activities
+      },
+      error: (error) => {
+        console.error("Error", error)
+      }
+    })
+    this.userActivityService.getAllUserActivities().subscribe({
+      next: (activities) => {
+        this.userActivities = activities
+      },
+      error: (error) => {
+        console.error("Error", error)
+      }
+    })
+  };
 
 }
