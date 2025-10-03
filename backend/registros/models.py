9e9cb0232ddc3cc79be0651a1a8b530d79ecea2f
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-10-03 01:40:44 -0300

    Feat: Backend. Creación de API registros

--- backend/registros/models.py
+++ backend/registros/models.py
@@ -1,3 +1,17 @@
+# backend/registros/models.py
 from django.db import models
+from usuarios.models import Usuario  
+from comisiones.models import Comision
 
 # Create your models here.
+
+class Registro(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="registros")
+    comision = models.ForeignKey(Comision, on_delete=models.CASCADE, related_name="registros")
+    fecha_inscripcion = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('usuario', 'comision') 
+
+    def __str__(self):
+        return f"{self.usuario.nombre} -> {self.comision.actividad.nombre}"
--- backend/registros/views.py
+++ backend/registros/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# backend/registros/views.py
+from rest_framework import viewsets
+from .models import Registro
+from .serializers import RegistroSerializer
 
-# Create your views here.
+class RegistroViewSet(viewsets.ModelViewSet):
+    queryset = Registro.objects.all()
+    serializer_class = RegistroSerializer
