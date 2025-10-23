e10792b71c0a4e9bdab33b52bb7c1029db215d8e
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-10-23 00:42:55 -0300

    fix: eliminado jwt

--- backend/usuarios/serializers.py
+++ backend/usuarios/serializers.py
@@ -2,17 +2,19 @@
 from rest_framework import serializers
 from .models import Usuario
 from django.contrib.auth.password_validation import validate_password
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class RegistroSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False, validators=[validate_password])
 
     class Meta:
         model = Usuario
-        fields = ["id", "dni", "nombre", "apellido", "email", "telefono", "sexo", "edad", "tipo", "is_active", "password"]  # ✅ Agregar password
+        fields = [
+            "id", "dni", "nombre", "apellido", "email", 
+            "telefono", "sexo", "fecha_nacimiento", "is_active", "password"
+        ]
         read_only_fields = ["id"]
         extra_kwargs = {
-            'password': {'write_only': True}  
+            'password': {'write_only': True}
         }
 
     def create(self, validated_data):
@@ -23,36 +25,10 @@ class RegistroSerializer(serializers.ModelSerializer):
             email=validated_data["email"],
             telefono=validated_data.get("telefono"),
             sexo=validated_data.get("sexo", ""),
-            edad=validated_data.get("edad"),
+            fecha_nacimiento=validated_data.get("fecha_nacimiento"),
+            tipo="usuario"
         )
         if 'password' in validated_data:
             usuario.set_password(validated_data["password"])
         usuario.save()
         return usuario
-
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    # Cambiar el campo de username a email
-    username_field = 'email'
-    
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token["nombre"] = user.nombre
-        token["apellido"] = user.apellido
-        token["tipo"] = user.tipo
-        return token
-
-    def validate(self, attrs):
-        # Llamar al validate del padre con email como username
-        data = super().validate(attrs)
-        
-        # Agregar información del usuario a la respuesta
-        data['user'] = {
-            'id': self.user.id,
-            'nombre': self.user.nombre,
-            'apellido': self.user.apellido,
-            'email': self.user.email,
-            'tipo': self.user.tipo,
-        }
-        
-        return data
